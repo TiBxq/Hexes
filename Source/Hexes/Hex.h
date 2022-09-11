c23d7fc5aae8f79ae2384a0f81ffdbf1b62226ee
Bogdan <savenko.bogdan@gmail.com>
2022-09-11 02:45:29 +0200

    implemented function to draw line on a hex grid

--- Source/Hexes/Hex.h
+++ Source/Hexes/Hex.h
@@ -6,6 +6,53 @@
 #include "UObject/NoExportTypes.h"
 #include "Hex.generated.h"
 
+struct FHexCube
+{
+	int32 q, r, s;
+
+	FHexCube(int32 q_, int32 r_, int32 s_) : q(q_), r(r_), s(s_) {}
+
+	//FHex ToHex() const { return FHex(q, r); }
+};
+
+struct FHexCubeFloat
+{
+	float q, r, s;
+
+	FHexCubeFloat(float q_, float r_, float s_) : q(q_), r(r_), s(s_) {}
+
+	FHexCubeFloat(const FHexCube& From) : q(From.q), r(From.r), s(From.s) {}
+
+	FHexCube Round() const
+	{
+		float rounded_q = FMath::RoundToFloat(q);
+		float rounded_r = FMath::RoundToFloat(r);
+		float rounded_s = FMath::RoundToFloat(s);
+
+		float diff_q = FMath::Abs(q - rounded_q);
+		float diff_r = FMath::Abs(r - rounded_r);
+		float diff_s = FMath::Abs(s - rounded_s);
+
+		if (diff_q > diff_r && diff_q > diff_s)
+			rounded_q = -rounded_r - rounded_s;
+		else if (diff_r > diff_q && diff_r > diff_s)
+			rounded_r = -rounded_q - rounded_s;
+		else
+			rounded_s = -rounded_q - rounded_r;
+
+		return FHexCube(rounded_q, rounded_r, rounded_s);
+	}
+
+	FHexCubeFloat Lerp(const FHexCubeFloat& Other, float Alpha) const
+	{
+		return FHexCubeFloat(
+			FMath::Lerp<float>(q, Other.q, Alpha),
+			FMath::Lerp<float>(r, Other.r, Alpha),
+			FMath::Lerp<float>(s, Other.s, Alpha)
+		);
+	}
+};
+
 USTRUCT(BlueprintType)
 struct HEXES_API FHex
 {
@@ -37,15 +84,20 @@ struct HEXES_API FHex
 		return FHex(q + other.q, r + other.r);
 	}
 
-	FHex Subtract(const FHex& other)
+	FHex Subtract(const FHex& other) const
 	{
 		return FHex(q - other.q, r - other.r);
 	}
 
-	int32 Distance(const FHex& other)
+	int32 Distance(const FHex& other) const
 	{
 		FHex dif = Subtract(other);
 		return (FMath::Abs(dif.q) + FMath::Abs(dif.r) + FMath::Abs(dif.GetS())) / 2;
+	}
+
+	FHexCube ToCube() const
+	{
+		return FHexCube(q, r, GetS());
 	}
 
 	static TArray<FHex> GetDirectionVectors()
@@ -65,5 +117,22 @@ struct HEXES_API FHex
 	FHex GetNeighbor(int32 direction) const
 	{
 		return Add(GetDirection(direction));
+	}
+
+	static TArray<FHex> GetLine(const FHex& Start, const FHex& End)
+	{
+		TArray<FHex> Result;
+		int32 N = Start.Distance(End);
+
+		FHexCubeFloat StartCube(Start.ToCube());
+		FHexCubeFloat EndCube(End.ToCube());
+
+		for (int32 i = 0; i < N; ++i)
+		{
+			FHexCube HexCube = (StartCube.Lerp(EndCube, 1.f / N * i)).Round();
+			Result.Add(FHex(HexCube.q, HexCube.r));
+		}
+
+		return Result;
 	}
 };
